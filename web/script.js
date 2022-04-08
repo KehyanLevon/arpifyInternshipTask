016d26d514822900a79c05b0c323af201f9120ae
KehyanLevon <mr.kehyan@mail.ru>
2022-04-09 00:14:25 +0400

    created requests

--- web/script.js
+++ web/script.js
@@ -1,15 +1,27 @@
-let myInfo = (
-{
-    "firstName":"Levon",
-    "lastName":"Kehyan",
-    "birthDay":"29-10-2002",
-    "gender":"male",
+document.addEventListener("DOMContentLoaded",  async function foo(){
+    let myInfo = (
+    {
+        firstName:"Levon",
+        lastName:"Kehyan",
+        birthDay:29-10-2002,
+        gender:"male",
+    });
+    let json = JSON.stringify(myInfo);
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "http://intern-2022.arpify.com/init");
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(json);
+
+    // const options = {
+    //   method:'POST',
+    //   headers:{
+    //     'Content-Type':'application/json'
+    //   },
+    //   body:JSON.stringify(myInfo)
+    // };
+    // const response = await fetch('http://intern-2022.arpify.com/init', options);
+    // console.log(response);
+    // const data = await response.json();
+    // console.log(data);
 });
-
-
-let json = JSON.stringify(myInfo);
-let xhr = new XMLHttpRequest();
-
-xhr.open("POST", 'http://intern-2022.arpify.com/init')
-xhr.setRequestHeader('Content-type', 'application/json');
-xhr.send(myInfo);
